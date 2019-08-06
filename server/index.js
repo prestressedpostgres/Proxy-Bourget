4533d0c699d49a7cb95a8f4504890cdb2aac8fa9
Jared Bourget <jared.m.bourget@gmail.com>
2019-08-05 18:59:37 -0600

    proxy for ec2

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// const nr = require('newrelic');
 const express = require('express');
 const path = require('path');
 const request = require('request');
@@ -11,46 +12,45 @@ server.use(express.static('../client'));
 
 const port = 5000;
 
-server.get('./bundle.js/:3000', (req, res) => {
-  request('http://localhost:3000/bundle.js', (error, response, body) => {
-    res.status(200).send(body);
-  })
-})
+// server.get('./bundle.js/:3000', (req, res) => {
+//   request('http://localhost:3000/bundle.js', (error, response, body) => {
+//     res.status(200).send(body);
+//   })
+// })
 
-server.get('/api/contact/:name', function(req, res) {
-  request(`http://localhost:2000/api/contact/:name`, (error, response, body) => {
-    // console.log(body)
+server.get('/pg/restaurant/:id', function(req, res) {
+  request(`http://localhost:4000/pg/restaurant/${req.body.id}`, (error, response, body) => {
     if (response.statusCode === 200) {
       res.status(200).send(body);
     }
   })
 });
 
-server.get('./bundle.js/:8081', (req, res) => {
-  request('http://localhost:8081/bundle.js', (error, response, body) => {
-    res.status(200).send(body);
-  })
-})
+// server.get('./bundle.js/:8081', (req, res) => {
+//   request('http://localhost:8081/bundle.js', (error, response, body) => {
+//     res.status(200).send(body);
+//   })
+// })
 
-server.use('/api/cities', (req, res) => {
-  request(`http://localhost:8081/api/cities`, (error, response, body) => {
-    // console.log(body)
-    if (response.statusCode === 200) {
-      res.status(200).send(body);
-    }
-  })
-})
+// server.use('/api/cities', (req, res) => {
+//   request(`http://localhost:8081/api/cities`, (error, response, body) => {
+//     // console.log(body)
+//     if (response.statusCode === 200) {
+//       res.status(200).send(body);
+//     }
+//   })
+// })
 
-server.use('/restaurant', (req, res) => {
-  console.log(req.query.name)
-  request(`http://localhost:8081/restaurant?name=${req.query.name}`, (error, response, body) => {
-    console.log(error)
-    if (response.statusCode === 200) {
-      // console.log(response)
-      res.status(200).send(body);
-    }
-  })
-})
+// server.use('/restaurant', (req, res) => {
+//   console.log(req.query.name)
+//   request(`http://localhost:8081/restaurant?name=${req.query.name}`, (error, response, body) => {
+//     console.log(error)
+//     if (response.statusCode === 200) {
+//       // console.log(response)
+//       res.status(200).send(body);
+//     }
+//   })
+// })
 
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
